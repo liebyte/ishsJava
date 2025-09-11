e86f6afac650a502d8bdc1a8dcc468d0d49cf08c
dalbodre333 <dalbodre333@gmail.com>
2025-09-11 19:36:08 +0900

    prime number v0.6

--- week05/src/PrimeGame.java
+++ week05/src/PrimeGame.java
@@ -16,14 +16,14 @@ public class PrimeGame {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean isPrime = true;
-        int number = sc.nextInt();
+        int[] numbers = {111, 977, 1, 2, 53, 1200}; //배역
 
-        if(isPrime(number)) {
-            System.out.println(number + " is a prime numnber~");
-        }else {
-            System.out.println(number + " is NOT prime number!");
+        for(int i=0; i<6; i++){
+            if(isPrime(numbers[i])) {
+                System.out.println(numbers[i] + " is a prime numnber~");
+            }else {
+                System.out.println(numbers[i] + " is NOT prime number!");
+            }
         }
     }
 }
