15216c5d9c0f4c8ea783af6e7556cda05382001c
dalbodre333 <dalbodre333@gmail.com>
2025-10-16 19:45:10 +0900

    v0.2 Pokemon game

--- week06/src/Pikachu.java
+++ week06/src/Pikachu.java
@@ -1,6 +1,6 @@
 public class Pikachu extends Pokemon {
     public Pikachu(int hp) {
-        super(name:"피카츄", hp); //부모클래스 생성자 호출
+        super("피카츄", hp); //부모클래스 생성자 호출
         System.out.println("피카피카~"); //호잇짜! 를 잇는 피카츄 태어나는 소리
     }
 
