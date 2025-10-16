66c6b5e257ab247fc6f9e7394592f96e0f4156ab
dalbodre333 <dalbodre333@gmail.com>
2025-10-16 20:09:06 +0900

    v0.3 Pokemon game

--- week06/src/Squirtle.java
+++ week06/src/Squirtle.java
@@ -1,12 +1,11 @@
 public class Squirtle extends Pokemon {
     public Squirtle(int hp) {
-        super("꼬부기", hp); //부모클래스 생성자 호출
-        System.out.println("거북거북~"); //호잇짜! 를 잇는 피카츄 태어나는 소리
+        super("꼬부기", hp);
+        System.out.println("거북거북~"); //무언가 마음이 거북해진다
     }
 
     @Override
     public void attack(Pokemon target) {
-        //System.out.println(getName() + " -> " + target.getName()); //name is private
-        System.out.println(name + " -> " + target.getName() + "에게 '거북목되기' 사용"); //name is protected
+        System.out.println(name + " -> " + target.getName() + "에게 '뒤통수에 물대포 맞아서 거북목되기' 사용");
     }
 }
