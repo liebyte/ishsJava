721f4921a150509527fc4e7a7c98021d1d7440c5
dalbodre333 <dalbodre333@gmail.com>
2025-11-06 19:44:50 +0900

    collection v0.2

--- week09/src/collections/CollectionDemo.java
+++ week09/src/collections/CollectionDemo.java
@@ -1,23 +1,35 @@
 package collections;
 
+import java.util.ArrayList;
+
 public class CollectionDemo {
     public static void main(String[] args) {
-        Fruit[] fruits = {
-                new Fruit("Kiwi", 3000),
-                new Fruit("Strawberry", 2500)
-        };
+        ArrayList<Fruit> fruits = new ArrayList<>();
+//        System.out.println(fruits.isEmpty()); // true
 
-        for(Fruit fruit : fruits) {
-            System.out.println(fruit.getName() + "(단가 : " + fruit.getPrice() ")");
-        }
+       fruits.add(new Fruit("Kiwi", 3000));
+       fruits.add(new Fruit("Strawberry", 2500));
+//       System.out.println(fruits.isEmpty()); // false
 
-        for int(int i=0; fruits.length; i++) {
-            System.out.println(fruits[i].getName() + "(단가 : " + fruits[i].getPrice() + ")")
-        }
 
-//        Fruit fruit1 = new Fruit("Kiwi", 3000);
-//        Fruit fruit1 = new Fruit("Strawberry", 2500);
-//        System.out.println(fruit1.getName() + "(단가 : " + fruit1.getPrice() + ")");
+//       Fruit[] fruits = {
+//               new Fruit("Kiwi", 3000),
+//               new Fruit("Strawberry", 2500)
+//       };
+
+        // array and arrayList
+//      for(Fruit fruit : fruits) {
+//          System.out.println(fruit.getName() + "(단가 : " + fruit.getPrice() + ")");
+//      }
+
+        // array only
+//       for (int i=0; i< fruits.length; i++) {
+//           System.out.println(fruits[i].getName() + "(단가 : " + fruits[i].getPrice() + ")");
+//       }
+
+        for (int i=0; i< fruits.size(); i++) {
+           System.out.println(fruits.get(i).getName() + "(단가 : " + fruits.get(i).getPrice() + ")");
+       }
 
 
     }
