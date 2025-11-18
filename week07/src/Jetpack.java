74ce0053aea88db44c47a1e7001701370eeede4f
dalbodre333 <dalbodre333@gmail.com>
2025-11-18 17:57:05 +0900

    v1.1.1 Pokemon game

--- week07/src/Jetpack.java
+++ week07/src/Jetpack.java
@@ -1,6 +1,6 @@
 public class Jetpack implements Flyable{
     @Override
-    public void fly() {
-        System.out.println("제트기가되. 쓩~");
+    public void fly(Pokemon pokemon) {
+        System.out.println(pokemon.getName() + "(이)가 제트기가 되. 쓩~");
     }
 }
