49e3119cda3198af381fe85fa1257dc55072183f
dalbodre333 <dalbodre333@gmail.com>
2025-11-18 19:38:05 +0900

    exception handling v0.4

--- week08/src/ExceptionTest.java
+++ week08/src/ExceptionTest.java
@@ -7,20 +7,16 @@ public class ExceptionTest {
         int numerator = 0;
         int denominator = 0;
 
-        try{
+        try {
             System.out.print("Input numerator : ");
             numerator = scanner.nextInt();
             System.out.print("Input denominator : ");
-            //double denominator = scanner.nextDouble();
             denominator = scanner.nextInt();
-        } catch (InputMismatchException err){
+            System.out.println(numerator / denominator);
+        }catch (InputMismatchException err){
             System.out.println("숫자로 입력하세요.");
-        }
-
-        if(denominator == 0){
-            System.out.println("분모에 0이 올 수 없습니다!"); // logical error
-        } else {
-            System.out.println(numerator/denominator);
+        }catch (ArithmeticException err){
+            System.out.println("분모에 0이 올 수 없습니다!");
         }
     }
 }
