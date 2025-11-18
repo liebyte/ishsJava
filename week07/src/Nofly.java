4a7885c285152f8dcc67a420c6373cc624e4e1ba
dalbodre333 <dalbodre333@gmail.com>
2025-11-18 16:11:53 +0900

    v0.9.1 Pokemon game

--- week07/src/Nofly.java
+++ week07/src/Nofly.java
@@ -1,6 +1,6 @@
 public class Nofly implements Flyable{
     @Override
-    public void fly(Pokemon pokemon) {
+    public void fly() {
         System.out.println("하늘을 날 수가 없습니다 엉엉");
     }
 }
--- week07/src/Pokemon.java
+++ week07/src/Pokemon.java
@@ -3,19 +3,36 @@ public abstract class Pokemon {
 //    protected String name;
     private int hp;
     protected int attackPower;
+    protected Flyable flyingTool; // has-a (Aggregration)
+
+
     public Pokemon(String name, int hp, int attackPower) {
         this.name  = name;
         this.hp = hp;
         this.attackPower = attackPower;
     }
 
-    public  String  getName() { return name; }
-    public int getHP() { return hp;}
+    public  String  getName() {
+        return name;
+    }
+    public int getHP() {
+        return hp;
+    }
     public int getAttackPower() {
         return attackPower;
     }
-    public void setHp(int hp) {this.hp = hp;}
 
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setFlyingTool(Flyable flyingTool) {
+        this.flyingTool = flyingTool;
+    }
+
+    public void performFlyable() {
+        this.flyingTool = fly();
+    }
 
     public abstract void attack(Pokemon target) ;
     public void receiveDamage(int damage){
